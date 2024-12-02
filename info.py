32577ea08745fd4d524b96bc70849fbe1fa8f4ec
Vishal1main <monsterr258258@gmail.com>
2024-12-02 14:43:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "")
+URL = environ.get("URL", "https://rear-penelopa-pilu-9931328a.koyeb.app/")
 
 # Admins, Channels & Users
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001854798365'))
